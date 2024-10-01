7aeae046c58ef2f7ee0d85dd04c71f822ec7b69f
Vlad Piatachenko <vlpiatachenko@gmail.com>
2024-10-01 15:31:36 +0300

    fix on units

--- src/test/java/com/fluffy/universe/utils/VelocityHelperTest.java
+++ src/test/java/com/fluffy/universe/utils/VelocityHelperTest.java
@@ -1,20 +1,32 @@
 package com.fluffy.universe.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class VelocityHelperTest {
-    /*  public static int calculateMaxPageNumber(int recordCount, int pageSize) {
-        return (int) Math.round(Math.ceil(recordCount / (double) pageSize));
-    }
-    */
+    @Order(1)
     @Test
-    void calculateMaxPageNumberTest(){
+    void calculateMaxPageNumberTest1(){
         assertEquals(1,VelocityHelper.calculateMaxPageNumber(2,4));
     }
+    @Order(3)
+    @Test
+    void calculateMaxPageNumberTest3(){
+        assertEquals(3,VelocityHelper.calculateMaxPageNumber(9,3));
+    }
+    @Order(2)
     @Test
     void generatePaginationLinkTest(){
         assertEquals("localhost/?page=2&size=4",VelocityHelper.generatePaginationLink("localhost/",2,4));
+    }
+    @Order(4)
+    @Test
+    void calculateMaxPageNumberTest4(){
+        assertThrows(NumberFormatException.class, () -> {
+            VelocityHelper.calculateMaxPageNumber(10, Integer.parseInt("a"));
+        });
     }
 }
