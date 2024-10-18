fb259895a55062cc894cc5520cd5f77fde16912f
Vlad Piatachenko <vlpiatachenko@gmail.com>
2024-10-18 23:26:54 +0300

    starter file for one feature

--- src/test/java/com/fluffy/universe/e2e/CucumberTest.java
+++ src/test/java/com/fluffy/universe/e2e/CucumberTest.java
@@ -6,10 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/Registration.feature",
         glue = "com.fluffy.universe.e2esteps",
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true // Makes console output readable
 )
-public class CucumberTestRunner {
+public class CucumberTest {
 }
